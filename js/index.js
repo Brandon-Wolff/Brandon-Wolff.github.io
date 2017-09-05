74f5901889c4bbbccb03dc808585cc49e18f014f
Brandon Wolff <brandon.wolff@hotmail.com>
2017-09-05 18:35:45 -0400

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('.navbar li').click(function(e) {
+  $('.navbar li.active').removeClass('active');
+  var $this = $(this);
+  if (!$this.hasClass('active')) {
+    $this.addClass('active');
+  }
+})
+
+$('body').scrollspy({
+  target: '#main-nav'
+});
